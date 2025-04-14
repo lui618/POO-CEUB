53720e5d2e7f14a9e30200d8bdc93703e930d8c4
Luis Campelo <luisfilipecab@gmail.com>
2025-04-14 09:36:13 -0300

    AVA2-IMC – Análise do IMC

--- AVA2-IMC/Pessoa.java
+++ AVA2-IMC/Pessoa.java
@@ -4,36 +4,6 @@ public class Pessoa{
     private double altura;
 
     
-    public String getNome() {
-        return nome;
-    }
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
-    public double getPeso() {
-        return peso;
-    }
-
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
-
-    public double getAltura() {
-        return altura;
-    }
-
-
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
-
-
     public Pessoa(String nome, double peso, double altura) {
         this.nome = nome;
         this.peso = peso;
